0d764f0901b9fdd73d5b334387ec19250b077425
Орехов Артем <orehouse@ya.ru>
2019-09-05 12:57:17 +0300

    addOrUpdateArrayElementWithKey added to utility

--- src/shared/utility.js
+++ src/shared/utility.js
@@ -1,3 +1,20 @@
 export const updateObject = (object, updatedFields) => {
   return { ...object, ...updatedFields };
 };
+
+export const addOrUpdateArrayElementWithKey = (
+  originalArray,
+  key,
+  valueToUpdate
+) => {
+  const shallowCopyOfArray = [...originalArray];
+  const indexOfElement = shallowCopyOfArray.findIndex(
+    el => el[key] === valueToUpdate[key]
+  );
+  if (indexOfElement > 0) {
+    shallowCopyOfArray[indexOfElement] = valueToUpdate;
+  } else {
+    shallowCopyOfArray.push(valueToUpdate);
+  }
+  return shallowCopyOfArray;
+};
