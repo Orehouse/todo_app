dedf5bd7ff18feb30f46122e92f3d9a84ecb019b
Орехов Артем <orehouse@ya.ru>
2019-09-05 18:30:57 +0300

    Hint added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Row, Col, Container } from "reactstrap";
+import { Row, Col, Container, Alert } from "reactstrap";
 import AddTodoForm from "./components/AddTodoForm/AddTodoForm";
 import TodoList from "./components/TodoList/TodoList";
 
@@ -14,6 +14,9 @@ const App = props => {
           <AddTodoForm />
         </Col>
         <Col md={12}>
+          <Alert color="primary">
+            To change your TODO, please double-click on its title.
+          </Alert>
           <TodoList />
         </Col>
       </Row>
