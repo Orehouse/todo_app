db9d88ba36e8ed0cfbb11d25c367b67ec3fc5e0b
Орехов Артем <orehouse@ya.ru>
2019-09-05 16:14:47 +0300

    Show completed status and "delete" text is button now

--- src/components/TodoList/TodoItem/TodoItem.js
+++ src/components/TodoList/TodoItem/TodoItem.js
@@ -1,18 +1,32 @@
 import React from "react";
 import { connect } from "react-redux";
+import { Button, Col, FormGroup, Input, Label } from "reactstrap";
 
 import * as actions from "../../../store/actions/index";
 
 const TodoItem = props => {
   return (
-    <tr>
-      <td>{props.title}</td>
+    <tr className={props.isCompleted ? "table-secondary" : ""}>
+      <td>
+        <FormGroup check>
+          <Label check>
+            <Input type="checkbox" checked={props.isCompleted} /> &#8203;
+          </Label>
+        </FormGroup>
+      </td>
       <td
-        onClick={() => {
-          props.onRemoveTodo(props.id);
-        }}
+        style={{ textDecoration: props.isCompleted ? "line-through" : "none" }}
       >
-        Delete
+        {props.title}
+      </td>
+      <td>
+        <Button
+          close
+          onClick={event => {
+            event.preventDefault();
+            props.onRemoveTodo(props.id);
+          }}
+        />
       </td>
     </tr>
   );
--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -10,10 +10,23 @@ const TodoList = props => {
     props.onFetchTodoList();
   }, []);
 
+  const getSortedTodoList = unsortedList => {
+    return [...unsortedList].sort((a, b) => {
+      const titleA = a.title.toUpperCase();
+      const titleB = b.title.toUpperCase();
+      return titleA < titleB ? -1 : titleA > titleB ? 1 : 0;
+    });
+  };
+
   const todoList =
     props.todoList && props.todoList.length > 0 ? (
-      props.todoList.map(todo => (
-        <TodoItem key={todo.id} title={todo.title} id={todo.id} />
+      getSortedTodoList(props.todoList).map(todo => (
+        <TodoItem
+          key={todo.id}
+          title={todo.title}
+          id={todo.id}
+          isCompleted={todo.isCompleted}
+        />
       ))
     ) : (
       <tr className="text-center">Your TODO list is empty!</tr>
@@ -23,8 +36,9 @@ const TodoList = props => {
     <Table>
       <thead>
         <tr>
+          <th>Completed</th>
           <th>Title</th>
-          <th>Actions</th>
+          <th></th>
         </tr>
       </thead>
       <tbody>{todoList}</tbody>
