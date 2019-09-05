c712eb0f9e4d61152fa04a0371f363ef01911c01
Орехов Артем <orehouse@ya.ru>
2019-09-05 13:13:12 +0300

    todoApi fix localStorage error

--- src/api/todoApi.js
+++ src/api/todoApi.js
@@ -12,14 +12,15 @@ export async function fetchTodoListAsync() {
 export async function putTodoAsync(todo) {
   return new Promise(resolve => {
     setTimeout(() => {
-      const todoList = JSON.parse(localStorage.getItem("todoList"));
+      let todoList = JSON.parse(localStorage.getItem("todoList"));
+      todoList = todoList ? todoList : [];
       if (todo.id) {
         todoList[todoList.findIndex(el => el.id === todo.id)] = todo;
       } else {
         todo = { ...todo, id: uniqid() };
         todoList.push(todo);
       }
-      localStorage.setItem("todoList", todoList);
+      localStorage.setItem("todoList", JSON.stringify(todoList));
       resolve(todo);
     }, 500);
   });
@@ -30,7 +31,7 @@ export async function deleteTodoAsync(id) {
     setTimeout(() => {
       const todoList = JSON.parse(localStorage.getItem("todoList"));
       const updatedTodoList = todoList.filter(el => el.id !== id);
-      localStorage.setItem("todoList", updatedTodoList);
+      localStorage.setItem("todoList", JSON.stringify(updatedTodoList));
       resolve(id);
     }, 500);
   });
