684d48fa372ac8db81abca283343771839b83c5a
Орехов Артем <orehouse@ya.ru>
2019-09-05 16:53:57 +0300

    Mark todo as completed implemented

--- src/components/TodoList/TodoItem/TodoItem.js
+++ src/components/TodoList/TodoItem/TodoItem.js
@@ -1,30 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
-import { Button, Col, FormGroup, Input, Label } from "reactstrap";
+import { Button, FormGroup, Input, Label } from "reactstrap";
+import { updateObject } from "../../../shared/utility";
 
 import * as actions from "../../../store/actions/index";
 
 const TodoItem = props => {
+  const [todoData, setTodoData] = useState({
+    id: props.id,
+    title: props.title,
+    isCompleted: props.isCompleted
+  });
+  const [todoNewTitle, setTodoNewTitle] = useState(props.title);
+
+  const onCompleteChangedHandler = event => {
+    const updatedTodo = updateObject(todoData, {
+      isCompleted: event.target.checked
+    });
+    setTodoData(updatedTodo);
+    props.onTodoUpdated(updatedTodo);
+  };
+
   return (
-    <tr className={props.isCompleted ? "table-secondary" : ""}>
+    <tr className={todoData.isCompleted ? "table-secondary" : ""}>
       <td>
         <FormGroup check>
           <Label check>
-            <Input type="checkbox" checked={props.isCompleted} /> &#8203;
+            <Input
+              type="checkbox"
+              checked={todoData.isCompleted}
+              onChange={onCompleteChangedHandler}
+            />{" "}
+            &#8203;
           </Label>
         </FormGroup>
       </td>
-      <td
-        style={{ textDecoration: props.isCompleted ? "line-through" : "none" }}
-      >
-        {props.title}
+      <td>
+        <span
+          style={{
+            textDecoration: todoData.isCompleted ? "line-through" : "none"
+          }}
+        >
+          {todoData.title}
+        </span>
       </td>
       <td>
         <Button
           close
           onClick={event => {
             event.preventDefault();
-            props.onRemoveTodo(props.id);
+            props.onRemoveTodo(todoData.id);
           }}
         />
       </td>
@@ -34,7 +59,8 @@ const TodoItem = props => {
 
 const mapDispatchToProps = dispatch => {
   return {
-    onRemoveTodo: id => dispatch(actions.removeTodo(id))
+    onRemoveTodo: id => dispatch(actions.removeTodo(id)),
+    onTodoUpdated: todo => dispatch(actions.addOrEditTodo(todo))
   };
 };
 
